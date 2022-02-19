d95b7a65e7c0176d8d06253f7691bff6760458e5
Juan Montoya <juanmont241@ed.amdsb.ca>
2022-02-19 13:50:09 -0500

    Changes

--- SimonSays/Form1.cs
+++ SimonSays/Form1.cs
@@ -14,6 +14,7 @@ namespace SimonSays
 {
     public partial class Form1 : Form
     {
+        public static List<int> numPattern = new List<int>();
         //TODO: create a List to store the pattern. Must be accessable on other screens
 
         public Form1()
@@ -21,9 +22,47 @@ namespace SimonSays
             InitializeComponent();
         }
 
+        public static void ChangeScreen(object sender, UserControl next)
+        {
+            Form f; // will either be the sender or parent of sender 
+
+            if (sender is Form)
+            {
+                f = (Form)sender;    //f is sender
+            }
+
+            else
+            {
+                UserControl current = (UserControl)sender;  //create UserControl from sender 
+                f = current.FindForm();                     //find Form UserControl is on 
+                f.Controls.Remove(current);                 //remove current UserControl
+            }
+
+            // add the new UserControl to the middle of the screen and focus on it 
+            next.Location = new Point((f.ClientSize.Width - next.Width) / 2,
+                (f.ClientSize.Height - next.Height) / 2);
+
+            f.Controls.Add(next);
+            next.Focus();
+        }
+
+        public static void ChangeScreen(UserControl current, UserControl next)
+        {
+
+            Form f = current.FindForm();
+            f.Controls.Remove(current);
+
+            next.Location = new Point((f.ClientSize.Width - next.Width) / 2,
+                (f.ClientSize.Height - next.Height) / 2);
+
+            next.Focus();
+            f.Controls.Add(next);
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
             //TODO: Launch MenuScreen
+            Form1.ChangeScreen(this, new MenuScreen());
+
         }
     }
 }
--- SimonSays/GameScreen.cs
+++ SimonSays/GameScreen.cs
@@ -15,7 +15,13 @@ namespace SimonSays
     public partial class GameScreen : UserControl
     {
         //TODO: create guess variable to track what part of the pattern the user is at
+        int guessNumber = 0;
+        Random randGen = new Random();
 
+        int oneNumber = 0;
+        int twoNumber = 1;
+        int threeNumber = 2;
+        int fourNumber = 3;
 
         public GameScreen()
         {
@@ -25,15 +31,64 @@ namespace SimonSays
         private void GameScreen_Load(object sender, EventArgs e)
         {
             //TODO: clear the pattern list from form1, refresh, pause for a bit, and run ComputerTurn()
+            Form1.numPattern.Clear();
+            Refresh();
+            Thread.Sleep(1000);
+            ComputerTurn();
         }
 
         private void ComputerTurn()
         {
             //TODO: get rand num between 0 and 4 (0, 1, 2, 3) and add to pattern list
+            guessNumber = randGen.Next(0, 4);
+            Form1.numPattern.Add(guessNumber);
 
             //TODO: create a for loop that shows each value in the pattern by lighting up approriate button
+            for (int i = 0; i < Form1.numPattern.Count(); i++)
+            {
+                if (oneNumber == Form1.numPattern[i])
+                {
+                    greenButton.BackColor = Color.LimeGreen;
+                    Refresh();
+                    //Place Sound
+                    Thread.Sleep(500);
+                    greenButton.BackColor = Color.ForestGreen;
+                    Refresh();
+                }
+
+                else if (twoNumber == Form1.numPattern[i])
+                {
+                    redButton.BackColor = Color.Firebrick;
+                    Refresh();
+                    //Place Sound
+                    Thread.Sleep(500);
+                    redButton.BackColor = Color.DarkRed;
+                    Refresh();
+                }
+
+                else if (threeNumber == Form1.numPattern[i])
+                {
+                    yellowButton.BackColor = Color.Yellow;
+                    Refresh();
+                    //Place Sound
+                    Thread.Sleep(500);
+                    yellowButton.BackColor = Color.Goldenrod;
+                    Refresh();
+                }
+
+                else if (fourNumber == Form1.numPattern[i])
+                {
+                    blueButton.BackColor = Color.RoyalBlue;
+                    Refresh();
+                    //Place Sound
+                    Thread.Sleep(500);
+                    blueButton.BackColor = Color.DarkBlue;
+                    Refresh();
+                }
+            }
 
             //TODO: get guess index value back to 0
+            guessNumber = 0;
         }
 
         public void GameOver()
@@ -41,19 +96,96 @@ namespace SimonSays
             //TODO: Play a game over sound
 
             //TODO: close this screen and open the GameOverScreen
+            Form1.ChangeScreen(this, new GameOverScreen());
 
         }
 
         //TODO: create one of these event methods for each button
         private void greenButton_Click(object sender, EventArgs e)
         {
+            if (Form1.numPattern[guessNumber] == 0)
+            {
+                greenButton.BackColor = Color.LimeGreen;
+                Refresh();
+                //Place Sound
+                Thread.Sleep(500);
+                greenButton.BackColor = Color.ForestGreen;
+                Refresh();
+                guessNumber++;
+            }
+
+            else
+            {
+                GameOver();
+            }
+
+            if (guessNumber == Form1.numPattern.Count)
+                //Play Sound
+                Thread.Sleep(500);
+                guessNumber = 0;
+                ComputerTurn();
+        }
+
+        private void redButton_Click(object sender, EventArgs e)
+        {
+            if (Form1.numPattern[guessNumber] == 1)
+            {
+                redButton.BackColor = Color.Firebrick;
+                Refresh();
+                //Place Sound
+                Thread.Sleep(500);
+                redButton.BackColor = Color.DarkRed;
+                Refresh();
+                guessNumber++;
+            }
+            else
+            {
+                GameOver();
+            }
+        }
+
+        private void yellowButton_Click(object sender, EventArgs e)
+        {
+            if (Form1.numPattern[guessNumber] == 2)
+            {
+                yellowButton.BackColor = Color.Yellow;
+                Refresh();
+                //Place Sound
+                Thread.Sleep(500);
+                yellowButton.BackColor = Color.Goldenrod;
+                Refresh();
+                guessNumber++;
+            }
+            else
+            {
+                GameOver();
+            }
+        }
+
+        private void blueButton_Click(object sender, EventArgs e)
+        {
+            if (Form1.numPattern[guessNumber] == 3)
+            {
+                blueButton.BackColor = Color.RoyalBlue;
+                Refresh();
+                //Place Sound
+                Thread.Sleep(500);
+                blueButton.BackColor = Color.DarkBlue;
+                Refresh();
+                guessNumber++;
+            }
+            else
+            {
+                GameOver();
+            }
+
             //TODO: is the value at current guess index equal to a green. If so:
-                // light up button, play sound, and pause
-                // set button colour back to original
-                // add one to the guess index
-                // check to see if we are at the end of the pattern. If so:
-                    // call ComputerTurn() method
-                // else call GameOver method
+            // light up button, play sound, and pause
+            // set button colour back to original
+            // add one to the guess index
+            // check to see if we are at the end of the pattern. If so:
+            // call ComputerTurn() method
+            // else call GameOver method
         }
     }
 }
